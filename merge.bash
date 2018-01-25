b92767e83d33f81d5ca96c685a642ade34932c4e
Erik Tjong Kim Sang <erikt@xs4all.nl>
2018-01-25 18:31:34 +0100

    removed sentence-initial constraint

--- merge.bash
+++ merge.bash
@@ -4,9 +4,9 @@
 # 20180122 erikt(at)xs4all.nl
 
 # keep labels 5, 11 and 12
-sed 's/^__label__[123] /__label__1+2+3 /' |\
-   sed 's/^__label__[67] /__label__6+7 /' |\
-   sed 's/^__label__[489] /__label__4+8+9+10 /' |\
-   sed 's/^__label__10 /__label__4+8+9+10 /' 
+sed 's/__label__[123] /__label__1+2+3 /' |\
+   sed 's/__label__[67] /__label__6+7 /' |\
+   sed 's/__label__[489] /__label__4+8+9+10 /' |\
+   sed 's/__label__10 /__label__4+8+9+10 /' 
 
 exit 0
