b7948f0d562becc91c327bf1e1da20ff462b5327
Erik Tjong Kim Sang <erikt@xs4all.nl>
2018-02-12 16:06:23 +0100

    processed other dataset

--- run.bash
+++ run.bash
@@ -4,16 +4,19 @@
 # 20180115 erikt(at)xs4all.nl
 
 FASTTEXT=$HOME/software/fastText/fasttext
-TRAIN=TRAIN.fasttext
-TEST=selected.fasttext
+TRAIN=TRAIN.fasttext.merged
+TEST=selected-201702.fasttext
+#TRAIN=TRAIN-TEST.fasttext
+#TEST=TEST.fasttext
 DIM=300
 MINCOUNT=5
 TMPFILE=run.$$.$RANDOM
 MODEL=$TMPFILE.model
 VECTORS=$TMPFILE.vectors
+WIKIVEC=/home/erikt/software/fastText/wiki.nl.vec
 
 $FASTTEXT supervised -input $TRAIN -output $MODEL -dim $DIM \
-    -minCount $MINCOUNT > /dev/null 2> /dev/null
+    -minCount $MINCOUNT -pretrainedVectors $WIKIVEC > /dev/null 2> /dev/null
 $FASTTEXT predict $MODEL.bin $TEST > $TEST.labels
 rm -f $MODEL.bin
 
